6e964bafd3e9d9ba2e709bd8ee38acd9ba6112d6
darkcryption <149880559+minecraft-beta@users.noreply.github.com>
2025-01-29 20:03:18 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = reqyire('path');
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, () => {
+    console.log('runnin on 8080')
+}) 
